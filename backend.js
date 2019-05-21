c40bb11df47f6e8d0e94811240da4d7d39690460
Grant <gth2103@columbia.edu>
2019-05-20 21:44:28 -0400

    implement backend, convert response to list, post list of obejcts to page

--- backend.js
+++ backend.js
@@ -1,37 +1,55 @@
+function displayArticles() {
+
+    // info from user input form
+    var searchTerm = $("#search-term").val().trim();
+    var startYear = $("#start-year").val().trim();
+    var endYear = $("#end-year").val().trim();
+        
+    // making the API call
+    var api = "6o2NDMTAd7Dis3U3TT5jo5aMTEQz3aSU";
+
+    // restricting results to source
+    var fq = "source:('The New York Times')"
+
+    // nytimes api information request
+    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&" + fq + "&begin_date=" + startYear + "0101&end_date=" + endYear + "1231&page=0&api-key=" + api
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).done(function (response) {
+        console.log(response);
+        assignToDOM(response);
+    })
+};
+
+function assignToDOM(response) {
+
+    var articles = response.response.docs;
+
+    var count = $( "#records option:selected" ).val();
+
+    for(var i = 0; i < count; i++){
+
+        var headline = articles[i].headline.main
+        var author = articles[i].byline.original
+        var section = articles[i].news_desk
+        var pub_date = articles[i].pub_date
+        var url = articles[i].web_url
+
+        var result = '<div class="article-item"><p class="headline"><h5>' + headline + '</h5></p><p class="author">' +  author + '</p><p class="section">Section: ' + section + '</p><p class="pub-date">' + pub_date + '</p><p class="url">' + url + '</p></div><br>'
+
+        $("#article-items").append(result)
+    }
+}
+
+function clear(){
+    $("#article-items").empty()
+}
+
 $(document).ready(function () {
     $(document).on("click", "#search", displayArticles);
-
-    function displayArticles() {
-        // pulling in info from form that user input
-        var searchTerm = $("#search-term").val().trim();
-        // var records = $("#records").val().trim();
-        var startYear = $("#start-year").val().trim();
-        var endYear = $("#end-year").val().trim();
-
-        // making the API call
-        var api = "6o2NDMTAd7Dis3U3TT5jo5aMTEQz3aSU";
-
-        var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&fq=source:('The New York Times')" + "&begin_date=" + startYear + "0101" + "&end_date=" + endYear + "1231" + "&page=0&api-key=" + api;
-
-        $.ajax({
-            url: queryUrl,
-            method: "GET",
-        }).then(function (response) {
-            console.log(response);
-            assignToDOM(response);
-        })
-
-        function assignToDOM(response) {
-            var headline = response.docs.headline;
-            var author = response.docs.byline.original;
-            var section = response.docs.section_name;
-            var publicationdate = response.docs.pub_date;
-            var url = response.docs.web - url;
-            $("#article-items").append(JSON.stringify(headline + author + section + publicationdate + url))
-        }
-
-    };
-
+    $(document).on("click", "#clear", clear);     
 });
 
 
